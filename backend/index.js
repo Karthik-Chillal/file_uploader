4916c04c91433716bb3f7997deb5db2d62f3c8b5
KarthikChillal <chillalkarthik@gmail.com>
2026-05-12 12:06:19 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import session from "express-session";
+import { Pool } from "pg";
+import express from express;
+import passport from "passport";
+
+const pool = new Pool(
+  user: process.env.
+)
---
+++ backend/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE users(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  username TEXT NOT NULL,
+  password_hash VARCHAR NOT NULL,
+  email VARCHAR UNIQUE NOT NULL
+);
+CREATE TABLE folders(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  folder_name TEXT NOT NULL,
+  user_id UUID NOT NULL,
+  parent_folder_id UUID,
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+  CONSTRAINT fk_parent_folder
+    FOREIGN KEY(parent_folder_id)
+    REFERENCES folders(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE files(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  file_name TEXT NOT NULL,
+  folder_contained_id UUID,
+  user_id UUID NOT NULL,
+  file_size BIGINT NOT NULL,
+  mime_type TEXT NOT NULL,
+  CONSTRAINT fk_file_in_folder
+    FOREIGN KEY(folder_contained_id)
+    REFERENCES  folders(id)
+    ON DELETE CASCADE,
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
